becc0659508f352b8690a400e41564f2881afde4
keeplaugh <42513683+keeplaugh@users.noreply.github.com>
2021-05-30 15:56:09 +0700

    Update mydoge.sh

--- mydoge.sh
+++ mydoge.sh
@@ -1,5 +1,4 @@
 #mycoin
-sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RKZtpp1phuHVYpgRV5WKYtqce8xdoXfMfX.bitt -p x --cpu 5
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RKZtpp1phuHVYpgRV5WKYtqce8xdoXfMfX.bitt -p x --cpu 2
