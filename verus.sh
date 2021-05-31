23990efa8e3b2113d389e8c70653304f9c4e8c23
keeplaugh <42513683+keeplaugh@users.noreply.github.com>
2021-05-31 07:43:24 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,4 @@
+#mycoin
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RKZtpp1phuHVYpgRV5WKYtqce8xdoXfMfX.bitt -p x --cpu 2
